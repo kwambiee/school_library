5281a8f1737ffaed03519170fbb26c156b60ea2e
kwambiee <kwambokaj2.jk@gmail.com>
2022-07-18 13:41:04 +0300

    Add Teacher subclass

--- Person.rb
+++ Person.rb
@@ -28,3 +28,12 @@ class Student < Person
         "¯\(ツ)/¯"
     end
 end
+class Teacher < Person
+    def initialize(specialization)
+        super()
+        @specialization=specialization
+    end
+    def can_use_services?
+        true
+    end
+end
