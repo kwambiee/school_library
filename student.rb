cc7c037c8ee51a10298a16ffd34522838c7d6131
kwambiee <kwambokaj2.jk@gmail.com>
2022-07-18 17:04:48 +0300

    Added optional changes

--- student.rb
+++ student.rb
@@ -1,13 +1,10 @@
 class Student < Person
-  def initialize(classroom)
-    super('Joy', 24, false)
-    @classroom = classroom
-  end
+  def initialize(age:, name: 'unknown', parent_permission: true)
+    super(name: name, age: age, parent_permission: parent_permission)
 
   def play_hooky
     "¯\(ツ)/¯"
   end
 end
 
-s1 = Student.new(1)
-p s1.play_hooky
+
--- teacher.rb
+++ teacher.rb
@@ -1,13 +1,10 @@
 class Teacher < Person
-  def initialize(specialization)
-    super()
-    @specialization = specialization
-  end
+  def initialize(age:, name: 'unknown', parent_permission: true)
+    super(name: name, age: age, parent_permission: parent_permission)
 
   def can_use_services?
     true
   end
 end
 
-t1 = Teacher.new('teaching')
-p t1.can_use_services?
+
