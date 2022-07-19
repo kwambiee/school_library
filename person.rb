1604d98f82a6fe7b5910262648b67c183c4a60b5
kwambiee <kwambokaj2.jk@gmail.com>
2022-07-19 12:13:28 +0300

    Add spark operator

--- person.rb
+++ person.rb
@@ -2,12 +2,12 @@ class Person
   attr_reader :id
   attr_accessor :name, :age, :parent_permission
 
-  def initialize(age:, name: 'unknown', parent_permission: true)
-    @id = Random.rand(1..1000)
-    @name = name
-    @age = age
-    @parent_permission = parent_permission
-  end
+  def initialize(*args)
+        @id=Random.rand(1..1000)
+        @age=args[0]
+        @name=args[1] || "unknown"
+        @parent_permission= args[2].nil? ? true : args[2]
+    end
 
   def can_use_services?
     of_age? || @parent_permission
