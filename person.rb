022e822d4daad5baceec2f2546ef7a463e012bae
kwambiee <kwambokaj2.jk@gmail.com>
2022-07-19 12:19:35 +0300

    Add base decorator

--- person.rb
+++ person.rb
@@ -30,4 +30,15 @@ class Person
   end
 end
 
+class Decorator < Nameable
+    attr_accessor :nameable
+
+    def initialize(nameable)
+        @nameable= nameable
+    end
+    def correct_name
+        @nameable.correct_name
+    end
+end
+
 
