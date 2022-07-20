07c11fc68657e36229be49be5b9f61736003ef53
kwambiee <kwambokaj2.jk@gmail.com>
2022-07-20 12:16:42 +0300

    Add label to student

--- student.rb
+++ student.rb
@@ -1,6 +1,7 @@
 require './person'
 
 class Student < Person
+  attr_reader :label
   def initialize(age, classroom, name = 'unknown', parent_permission: true)
     super(age: age, name: name, parent_permission: parent_permission)
     @classroom = classroom
@@ -8,5 +9,10 @@ class Student < Person
 
   def play_hooky
     "¯\(ツ)/¯"
+  end
+
+  def label=(label)
+    @label=label
+    label.students.push(self) unless label.students.push.include?(self)
   end
 end
