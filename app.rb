e6fb6556f3077537f639e995c1e6299c3e1fab65
kwambiee <kwambokaj2.jk@gmail.com>
2022-07-21 11:38:41 +0300

    Add welcome method

---
+++ app.rb
@@ -0,0 +1,8 @@
+class App
+    def welcome
+        puts "Welcome to School Library App!"
+        puts ""
+        puts "Please choose an option by entering a number"
+        options
+    end
+end
