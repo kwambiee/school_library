613bf77505cbbbba07c666a94e53458beffa3122
kwambiee <kwambokaj2.jk@gmail.com>
2022-07-21 11:40:48 +0300

    Add options method

--- app.rb
+++ app.rb
@@ -5,4 +5,17 @@ class App
         puts "Please choose an option by entering a number"
         options
     end
+    def options
+        puts "
+        1 - List all books.
+        2 - List all people.
+        3 - Create a person.
+        4 - Create a book.
+        5 - Create a rental.
+        6 - List all rentals for a given person id.
+        7 - Exit
+        "
+        user_option=gets.chomp
+        get_option(user_option)
+    end
 end
