15e3e9dc26cefb667c9691691c9c67c08fd24951
kwambiee <kwambokaj2.jk@gmail.com>
2022-07-21 16:52:34 +0300

    Fix linters

--- app.rb
+++ app.rb
@@ -55,7 +55,7 @@ class App
 
   def other_options(user_option)
     if user_option == '7'
-      "Thanks for using School Library App!"
+      'Thanks for using School Library App!'
     else
       options
     end
