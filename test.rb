c9aca6d23c82a1ae7c00eaed6c7b8be170bb6dd6
kwambiee <kwambokaj2.jk@gmail.com>
2022-07-25 11:42:21 +0300

    Fix linters

--- test.rb
+++ test.rb
@@ -1,9 +1,8 @@
 require 'pry'
 
 def simple_cubing_tool(number)
-    num_cubed=number * number * number
-    puts "The answer is #{num_cubed}!"
-
+  num_cubed = number * number * number
+  puts "The answer is #{num_cubed}!"
 end
 
 simple_cubing_tool(5)
