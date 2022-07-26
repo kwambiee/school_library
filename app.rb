92bfad16a210a2626e7249c89b64d223c31b85e7
kwambiee <kwambokaj2.jk@gmail.com>
2022-07-26 11:41:23 +0300

    Write data to json file

--- app.rb
+++ app.rb
@@ -109,4 +109,17 @@ class App
       puts "Date: #{rental.date} Book: '#{rental.book.title}' by #{rental.book.author}" if rental.person.id == person_id
     end
   end
+
+  def write_json
+    File.open('books.json', 'w') do |file|
+      file.write(@books.to_json)
+    end
+    File.open('people.json', 'w') do |file|
+      file.write(@people.to_json)
+    end
+    File.open('rentals.json', 'w') do |file|
+      file.write(@rentals.to_json)
+    end
+  end
+
 end
